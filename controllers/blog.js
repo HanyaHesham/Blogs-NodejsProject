0907e6733bad5097cb92a70adf0912a3c56759e9
HanyaHesham <77938444+HanyaHesham@users.noreply.github.com>
2021-02-13 16:55:08 +0200

    update

--- controllers/blog.js
+++ controllers/blog.js
@@ -64,8 +64,11 @@ const addComment = (id, body)=>{
     return Blog.updateOne({_id: id}, {$addToSet: body}, {new: true}).exec();
 }
 
-const getBlogsByAuthor=(author) =>{
-    return Blog.find({author}).exec();
+// const getBlogsByAuthor=(author) =>{
+//     return Blog.find({author}).exec();
+// }
+const getBlogsByAuthor = (author) => {
+    return Blog.find(author).sort({ createdAt: 'desc'}).populate('author').exec();
 }
 
 
